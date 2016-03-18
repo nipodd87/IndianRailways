152c45396a242e23c9b244992ec8f8cfa54787e4
NitinPoddar <hitk.nitinpoddar@gmail.com>
2016-03-18 10:20:44 -0400

    Additional coding done for Live Station Status feature, new activity file created

--- app/src/main/java/com/nitz/studio/indianrailways/LiveStation.java
+++ app/src/main/java/com/nitz/studio/indianrailways/LiveStation.java
@@ -1,10 +1,15 @@
 package com.nitz.studio.indianrailways;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 /**
  * Created by nitinpoddar on 3/17/16.
  */
 public class LiveStation extends ActionBarActivity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livestation);
+    }
 }
